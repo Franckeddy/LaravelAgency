4824fbaa759c547643cc174d473a2f489de491b9
Franck Garcon <franck.garcon@gmail.com>
2023-07-14 07:28:49 +0200

    Add Form request / Details

--- resources/views/component/property/show.blade.php
+++ resources/views/component/property/show.blade.php
@@ -26,8 +26,21 @@
                             src="https://images.unsplash.com/photo-1456948927036-ad533e53865c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
                             class="aspect-square w-full rounded-xl object-cover"
                         />
+
+                        <img
+                            alt="Les Paul"
+                            src="https://images.unsplash.com/photo-1456948927036-ad533e53865c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
+                            class="aspect-square w-full rounded-xl object-cover"
+                        />
+
+                        <img
+                            alt="Les Paul"
+                            src="https://images.unsplash.com/photo-1456948927036-ad533e53865c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
+                            class="aspect-square w-full rounded-xl object-cover"
+                        />
                     </div>
                 </div>
+
                 <div class="sticky top-0">
                     @if($property->status === true)
                         <strong
@@ -40,69 +53,14 @@
                             A Vendre
                         </strong>
                     @endif
+
                     <div class="mt-8 flex justify-between">
                         <div class="max-w-[35ch] space-y-2">
-                            <h1 class="text-xl font-bold sm:text-2xl text-black">
+                            <h1 class="text-xl font-bold sm:text-2xl">
                                 {{ $property->title }}
                             </h1>
-{{--                            <p class="text-sm">Highest Rated Product</p>--}}
-{{--                            <div class="-ms-0.5 flex">--}}
-{{--                                <svg--}}
-{{--                                    class="h-5 w-5 text-yellow-400"--}}
-{{--                                    xmlns="http://www.w3.org/2000/svg"--}}
-{{--                                    viewBox="0 0 20 20"--}}
-{{--                                    fill="currentColor"--}}
-{{--                                >--}}
-{{--                                    <path--}}
-{{--                                        d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"--}}
-{{--                                    />--}}
-{{--                                </svg>--}}
-
-{{--                                <svg--}}
-{{--                                    class="h-5 w-5 text-yellow-400"--}}
-{{--                                    xmlns="http://www.w3.org/2000/svg"--}}
-{{--                                    viewBox="0 0 20 20"--}}
-{{--                                    fill="currentColor"--}}
-{{--                                >--}}
-{{--                                    <path--}}
-{{--                                        d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"--}}
-{{--                                    />--}}
-{{--                                </svg>--}}
-
-{{--                                <svg--}}
-{{--                                    class="h-5 w-5 text-yellow-400"--}}
-{{--                                    xmlns="http://www.w3.org/2000/svg"--}}
-{{--                                    viewBox="0 0 20 20"--}}
-{{--                                    fill="currentColor"--}}
-{{--                                >--}}
-{{--                                    <path--}}
-{{--                                        d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"--}}
-{{--                                    />--}}
-{{--                                </svg>--}}
-
-{{--                                <svg--}}
-{{--                                    class="h-5 w-5 text-yellow-400"--}}
-{{--                                    xmlns="http://www.w3.org/2000/svg"--}}
-{{--                                    viewBox="0 0 20 20"--}}
-{{--                                    fill="currentColor"--}}
-{{--                                >--}}
-{{--                                    <path--}}
-{{--                                        d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"--}}
-{{--                                    />--}}
-{{--                                </svg>--}}
-
-{{--                                <svg--}}
-{{--                                    class="h-5 w-5 text-gray-200"--}}
-{{--                                    xmlns="http://www.w3.org/2000/svg"--}}
-{{--                                    viewBox="0 0 20 20"--}}
-{{--                                    fill="currentColor"--}}
-{{--                                >--}}
-{{--                                    <path--}}
-{{--                                        d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"--}}
-{{--                                    />--}}
-{{--                                </svg>--}}
-{{--                            </div>--}}
                         </div>
+
                         <p class="text-lg font-bold">{{ number_format( $property->price, thousands_separator: ' ')}} €</p>
                     </div>
 
@@ -117,7 +75,6 @@
                     <form class="mt-8">
                         <fieldset>
                             <legend class="mb-1 text-sm font-medium text-black">Surface</legend>
-
                             <div class="flex flex-wrap gap-1">
                                 <label for="color_tt" class="cursor-pointer">
                                     <input
@@ -129,53 +86,148 @@
 
                                     <span
                                         class="inline-block rounded-full text-xs font-medium"
-                                    >{{ $property->surface }} m2
-                </span>
+                                    >{{ $property->surface }} m2</span>
                                 </label>
-                        </fieldset>
-
-                        <fieldset class="mt-4">
-                            <legend class="mb-1 text-sm font-medium text-black">Adresse</legend>
-
-                            <div class="flex flex-wrap gap-1">
-                                <label for="size_xs">
-                                    <span
-                                        class="inline-flex items-center justify-center text-xs font-medium"
-                                    >{{ $property->address }}, </span>
-                                </label>
-
-                                    <span
-                                        class="inline-flex items-center justify-center text-xs font-medium"
-                                    >{{ $property->city }}
-                </span>
-
-                                    <span
-                                        class="group inline-flex items-center justify-center text-xs font-medium"
-                                    >{{ $property->postal_code }}
-                </span>
                             </div>
                         </fieldset>
+                        <div class="flex gap-10">
+                        <fieldset class="mt-4">
+                            <legend class="mb-1 text-sm font-medium text-black">Pi&egrave;ces</legend>
+                            <div class="flex flex-wrap gap-1">
+                                <label for="color_tt" class="cursor-pointer">
+                                    <input
+                                        type="radio"
+                                        name="color"
+                                        id="color_tt"
+                                        class="peer sr-only"
+                                    />
 
-{{--                        <div class="mt-8 flex gap-4">--}}
-{{--                            <div>--}}
-{{--                                <label for="quantity" class="sr-only">Qty</label>--}}
+                                    <span
+                                        class="inline-block rounded-full text-xs font-medium"
+                                    >{{ $property->rooms }}</span>
+                                </label>
+                            </div>
+                        </fieldset>
+                        <fieldset class="mt-4">
+                            <legend class="mb-1 text-sm font-medium text-black">Chambres</legend>
+                            <div class="flex flex-wrap gap-1">
+                                <label for="color_tt" class="cursor-pointer">
+                                    <input
+                                        type="radio"
+                                        name="color"
+                                        id="color_tt"
+                                        class="peer sr-only"
+                                    />
 
-{{--                                <input--}}
-{{--                                    type="number"--}}
-{{--                                    id="quantity"--}}
-{{--                                    min="1"--}}
-{{--                                    value="1"--}}
-{{--                                    class="w-12 rounded border-gray-200 py-3 text-center text-xs [-moz-appearance:_textfield] [&::-webkit-inner-spin-button]:m-0 [&::-webkit-inner-spin-button]:appearance-none [&::-webkit-outer-spin-button]:m-0 [&::-webkit-outer-spin-button]:appearance-none"--}}
-{{--                                />--}}
-{{--                            </div>--}}
+                                    <span
+                                        class="inline-block rounded-full text-xs font-medium"
+                                    >{{ $property->bedrooms }}</span>
+                                </label>
+                            </div>
+                        </fieldset>
+                            <fieldset class="mt-4">
+                                <legend class="text-sm font-medium text-black">Etage</legend>
+                                <div class="flex flex-wrap gap-1">
+                                    <label for="color_tt">
+                                        <input
+                                            type="radio"
+                                            name="color"
+                                            id="color_tt"
+                                            class="peer sr-only"
+                                        />
 
-{{--                            <button--}}
-{{--                                type="submit"--}}
-{{--                                class="block rounded bg-green-600 px-5 py-3 text-xs font-medium text-white hover:bg-green-500"--}}
-{{--                            >--}}
-{{--                                Add to Cart--}}
-{{--                            </button>--}}
-{{--                        </div>--}}
+                                        <span class="inline-block rounded-full text-xs font-medium">
+                                        {{ $property->floor ?? 'Rez de chaussée' }}
+                                    </span>
+                                    </label>
+                                </div>
+                            </fieldset>
+                        </div>
+                        <fieldset class="mt-4">
+                            <legend class="mb-1 text-sm font-medium text-black">Adresse</legend>
+                            <div class="flex flex-wrap gap-1">
+                                    <span class="inline-flex items-center justify-center text-xs font-medium">
+                                        {{ $property->address }},
+                                    </span>
+                                    <span class="inline-flex items-center justify-center text-xs font-medium">
+                                        {{ $property->city }}
+                                    </span>
+                                    <span class="group inline-flex items-center justify-center text-xs font-medium">
+                                        {{ $property->postal_code }}
+                                    </span>
+                            </div>
+                        </fieldset>
+                        <fieldset class="mt-4">
+                            @if($property->options->count() > 0)
+                                <legend class="mb-1 text-sm font-medium text-black">Sp&eacute;cificit&eacute;s</legend>
+                                @foreach($property->options as $option)
+                                    <div class="flex flex-wrap gap-1">
+                                        <label for="color_tt">
+                                            <input type="radio" name="color" id="color_tt" class="peer sr-only">
+                                            <span class="group inline-block rounded-full border px-3 py-1 text-xs font-medium peer-checked:bg-black peer-checked:text-white">
+                                        {{ $option->name }}
+                                    </span>
+                                        </label>
+                                    </div>
+                                @endforeach
+                            @endif
+                        </fieldset>
+                        <section class="bg-white">
+                            <div class="lg:grid lg:min-h-screen lg:grid-cols-12">
+                                <main class="flex lg:py-12 xl:col-span-12">
+                                    <div class="max-w-xl lg:max-w-3xl">
+                                        <form action="#" class="" method="POST">
+                                            @csrf
+                                            <div class="grid grid-cols-2 lg:gap-8">
+                                            @include('shared.input', [
+                                                'label' => 'First Name',
+                                                'name' => 'first_name',
+                                                'type' => 'text',
+                                                'placeholder' => 'Prénom',
+                                                'required' => true,
+                                                'autofocus' => true,
+                                            ])
+                                                @include('shared.input', [
+                                                    'label' => 'Last Name',
+                                                    'name' => 'last_name',
+                                                    'type' => 'text',
+                                                    'placeholder' => 'Nom',
+                                                    'required' => true,
+                                                ])
+                                            </div>
+                                            <div class="grid grid-cols-2 gap-4 lg:gap-8">
+                                                @include('shared.input', [
+                                                    'label' => 'Email',
+                                                    'name' => 'email',
+                                                    'type' => 'email',
+                                                    'placeholder' => 'Email',
+                                                    'required' => true,
+                                                    'autofocus' => true,
+                                                ])
+                                                @include('shared.input', [
+                                                    'label' => 'Phone',
+                                                    'name' => 'phone',
+                                                    'type' => 'text',
+                                                    'placeholder' => 'Téléphone',
+                                                    'required' => true,
+                                                ])
+                                            </div>
+                                            <div>
+                                                @include('shared.input', ['type' => 'textarea', 'placeholder' => 'Votre Message', 'name' => 'description'])
+                                            </div>
+                                            <div class="py-2 col-span-6 sm:flex sm:items-center sm:gap-4">
+                                                <button
+                                                    class="inline-block shrink-0 rounded-md border border-blue-600 bg-blue-600 px-12 py-3 text-sm font-medium text-white transition hover:bg-transparent hover:text-blue-600 focus:outline-none focus:ring active:text-blue-500"
+                                                >
+                                                    Nous contacter
+                                                </button>
+
+                                            </div>
+                                        </form>
+                                    </div>
+                                </main>
+                            </div>
+                        </section>
                     </form>
                 </div>
             </div>
